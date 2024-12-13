7535b2263010f918fb3d89612eb38c1003d73ca7
김유진 <kkieenex@naver.com>
2024-12-13 14:41:18 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test5',
+  webDir: 'dist'
+};
+
+export default config;
