e48fd4b16c22cb34b7a72c32c2e1c61c05006c91
김유진 <kkieenex@naver.com>
2024-12-13 15:34:34 +0900

    ddd

---
+++ src/views/main.vue
@@ -0,0 +1,13 @@
+<template>
+    <ion-page>
+      dddd
+    </ion-page>
+  </template>
+  
+  <script setup>
+  import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+  import { ellipse, square, triangle } from 'ionicons/icons';
+  
+  
+  </script>
+  
